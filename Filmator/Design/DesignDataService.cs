b8c072704cbe99f8883a8c98dbe8e97118a1d6de
Vorgnr <arnaud.cavat@gmail.com>
2013-12-03 12:58:16 +0100

    Premmier commit

---
+++ Filmator/Design/DesignDataService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Filmator.Model;
+
+namespace Filmator.Design {
+    public class DesignDataService : IDataService {
+        public void GetData(Action<DataItem, Exception> callback) {
+            // Use this to create design time data
+
+            var item = new DataItem("Welcome to MVVM Light [design]");
+            callback(item, null);
+        }
+    }
+}
