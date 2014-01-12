c6059c3f402f2d5f983df699b8a9fab3a2d6506d
vorgnr <vorgnr@hotmail.fr>
2014-01-12 22:23:44 +0100

    Adding Flashy color

---
+++ Filmator/Converters/DoubleToColorConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Filmator.Converters {
+    class DoubleToColorConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var num = value as double?;
+            var bc = new BrushConverter();
+            if (num == null || num < 4) return bc.ConvertFrom("#c0392b");
+            return bc.ConvertFrom(num < 7 ? "#f39c12" : "#27ae60");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
